7ce574804cc7e7a4c502b15fa0525fcd35f9e6e1
Nicky9319 <paarthsaxena2005@gmail.com>
2025-06-24 01:33:56 +0000

    created the process json file andb updated the services json file too changed the privieldged ports for the postgred db service

--- service_PostgresDBService/postgres-db-service.js
+++ service_PostgresDBService/postgres-db-service.js
@@ -10,7 +10,7 @@ function startService() {
 
     const httpServerPort = 20000;
     const httpServerHost = "127.0.0.1";
-    const httpServerPrivilegedIpAddress = ["192.168.0.1"];
+    const httpServerPrivilegedIpAddress = ["127.0.0.1"];
 
     const httpServer = new HTTP_SERVER(
         httpServerHost,
